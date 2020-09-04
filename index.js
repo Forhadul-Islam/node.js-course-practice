d3b187615adc01360c9fdb9a04f651cc1db9f454
Mizanur Rahaman <forhadulislam2100@gmial.com>
2020-09-05 01:49:45 +0600

    changeed the previous code using callback and closure

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const rect = require("./rectangal");
-const solveArea = (l, h) => {
-  console.log(`It is a rectangle of l = ${l} and h = ${h}`);
-  if (l <= 0 || h <= 0) {
-    console.log(
-      `The dimensions of a rectangle must be greater than zero: Here, l=${l} and h=${h}`
-    );
-  } else {
-    console.log(`The area of the rectangle is ${rect.area(l, h)}`);
-    console.log(`The perimeter of the rectangle is ${rect.perimeter(l, h)}`);
-  }
+const solveArea = (l, b) => {
+  rect(l, b, (err, rectangle) => {
+    if (err) {
+      console.log("err is,", err.message);
+    } else {
+      console.log(`The area of the rectangle is ${rectangle.area()}`);
+      console.log(`The perimeter of the rectangle is ${rectangle.perimeter()}`);
+    }
+  });
+  console.log("code is running ");
 };
 
 solveArea(2, 3);
--- rectangal.js
+++ rectangal.js
@@ -1,2 +1,23 @@
-exports.area = (x, y) => x * y;
-exports.perimeter = (x, y) => 2 * (x + y);
+module.exports = (x, y, callBack) => {
+  if (x <= 0 || y <= 0) {
+    setTimeout(
+      () =>
+        callBack(
+          new Error(
+            `The dimension of a rectangle must be greater than zero: here, length is ${x} and breath is ${y}`
+          ),
+          null
+        ),
+      4000
+    );
+  } else {
+    setTimeout(
+      () =>
+        callBack(null, {
+          area: () => x * y,
+          perimeter: () => 2 * (x + y),
+        }),
+      4000
+    );
+  }
+};
